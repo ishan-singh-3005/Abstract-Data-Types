be99cc155a9d53721bc3ff561f0b5a0bd6eaba22
ishan-singh-3005 <ishan423@gmail.com>
2022-05-18 20:28:18 -0400

    Created queue.py

---
+++ queue.py
@@ -0,0 +1,55 @@
+from typing import Optional, Any
+
+
+class Queue:
+    """A first-in-first-out (FIFO) queue of items.
+
+    Stores data in a first-in, first-out order. When removing an item from the
+    queue, the most recently-added item is the one that is removed.
+    """
+    # === Private Attributes ===
+    # _items:
+    #     The items stored in this stack. The end of the list represents
+    #     the top of the stack.
+    def __init__(self) -> None:
+        """Initialize a new empty queue."""
+        self._items = []
+
+    def is_empty(self) -> bool:
+        """Return whether this queue contains no items.
+
+        >>> q = Queue()
+        >>> q.is_empty()
+        True
+        >>> q.enqueue('hello')
+        >>> q.is_empty()
+        False
+        """
+        return self._items == []
+
+    def enqueue(self, item: Any) -> None:
+        """Add <item> to the back of this queue.
+        """
+        self._items.append(item)
+
+    def dequeue(self) -> Optional[Any]:
+        """Remove and return the item at the front of this queue.
+
+        Return None if this Queue is empty.
+        (We illustrate a different mechanism for handling an erroneous case.)
+
+        >>> q = Queue()
+        >>> q.enqueue('hello')
+        >>> q.enqueue('goodbye')
+        >>> q.dequeue()
+        'hello'
+        """
+        if not self._items:
+            raise EmptyQueueError
+        else:
+            return self._items.pop(0)
+
+
+class EmptyQueueError(Exception):
+    """Exception raised when an error occurs."""
+    pass
